4d56bda7df88936fae8c08c77a9e0726cc6e5ad3
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-03-22 18:34:23 +0100

    🔧 Removed usage of ArchAPI

--- forge/src/main/java/infinituum/fastconfigapi/forge/FastConfigAPIForge.java
+++ forge/src/main/java/infinituum/fastconfigapi/forge/FastConfigAPIForge.java
@@ -1,14 +1,11 @@
 package infinituum.fastconfigapi.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import infinituum.fastconfigapi.FastConfigAPI;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(FastConfigAPI.MOD_ID)
 public class FastConfigAPIForge {
     public FastConfigAPIForge() {
-        EventBuses.registerModEventBus(FastConfigAPI.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         FastConfigAPI.init();
     }
 }
