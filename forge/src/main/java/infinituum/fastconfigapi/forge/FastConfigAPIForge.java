b4a15847a88993275213eceb64df74d0891a2165
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-04-03 02:08:22 +0200

    🎉 New version! Removed Fabric / Forge dependency on common so that it can be published and used without weird files. Changed version. Closes #2. Updated API Java Docs and API functions.

--- forge/src/main/java/infinituum/fastconfigapi/forge/FastConfigAPIForge.java
+++ forge/src/main/java/infinituum/fastconfigapi/forge/FastConfigAPIForge.java
@@ -1,11 +1,15 @@
 package infinituum.fastconfigapi.forge;
 
-import infinituum.fastconfigapi.FastConfigAPI;
 import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Mod(FastConfigAPI.MOD_ID)
+@Mod(FastConfigAPIForge.MOD_ID)
 public class FastConfigAPIForge {
+    public static final String MOD_ID = "fastconfigapi";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     public FastConfigAPIForge() {
-        FastConfigAPI.init();
+        LOGGER.info("FastConfigAPI has been initialized!");
     }
 }
