1291e7d9b74d217a2c6085c24028826c3e368ff9
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-04-03 02:58:10 +0200

    🔧 NeoForge fix

--- neoforge/src/main/java/infinituum/fastconfigapi/forge/FastConfigAPINeoForge.java
+++ neoforge/src/main/java/infinituum/fastconfigapi/forge/FastConfigAPINeoForge.java
@@ -1,11 +1,15 @@
 package infinituum.fastconfigapi.forge;
 
-import infinituum.fastconfigapi.FastConfigAPI;
 import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Mod(FastConfigAPI.MOD_ID)
+@Mod(FastConfigAPINeoForge.MOD_ID)
 public class FastConfigAPINeoForge {
+    public static final String MOD_ID = "fastconfigapi";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     public FastConfigAPINeoForge() {
-        FastConfigAPI.init();
+        LOGGER.info("FastConfigAPI has been initialized!");
     }
 }
