06d4b3965f37ff85d0e6713fa54e11691984d9ea
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-04-03 02:08:22 +0200

    🎉 New version! Removed Fabric / Forge dependency on common so that it can be published and used without weird files. Changed version. Closes #2. Updated API Java Docs and API functions.

--- common/src/main/java/infinituum/fastconfigapi/api/FastConfig.java
+++ common/src/main/java/infinituum/fastconfigapi/api/FastConfig.java
@@ -5,13 +5,16 @@ import com.google.common.base.CaseFormat;
 import java.io.File;
 import java.nio.file.Path;
 
+/**
+ * Fast Config class.
+ */
 public class FastConfig {
     private final Path CONFIG_DIR_PATH;
     private FastConfig SUB_DIR_CONFIG;
 
     /**
-     * SimpleConfig constructor
-     * @param configDirectory Path to the ".minecraft/config" directory
+     * {@link FastConfig} constructor.
+     * @param configDirectory Path to the ".minecraft/config" directory (or custom).
      */
     public FastConfig(Path configDirectory) {
         this.CONFIG_DIR_PATH = configDirectory;
@@ -20,9 +23,10 @@ public class FastConfig {
     }
 
     /**
-     * SimpleConfig constructor
-     * @param configDirectory Path to the ".minecraft/config" directory
-     * @param subDirectoryName Name of the subdirectory (created if it doesn't exist)
+     * {@link FastConfig} constructor.
+     * @param configDirectory Path to the ".minecraft/config" directory (or custom).
+     * @param subDirectoryName Name of the subdirectory (created if it doesn't exist).
+     *                         This name is used to directly create a sub-FastConfig instance for files in a specific directory.
      */
     public FastConfig(Path configDirectory, final String subDirectoryName) {
         this.CONFIG_DIR_PATH = configDirectory;
@@ -31,48 +35,48 @@ public class FastConfig {
     }
 
     /**
-     * Gets the current Config target directory
-     * @return Path to the current Config target directory
+     * Gets the directory path this {@link FastConfig} is targeting.
+     * @return Path to the current Config target directory.
      */
     public Path getConfigDirPath() {
         return CONFIG_DIR_PATH;
     }
 
     /**
-     * Gets the subdirectory Config if one exists
-     * @return the subdirectory Config
+     * Gets the {@link FastConfig} associated with the subdirectory, if one exists.
+     * @return The subdirectory {@link FastConfig}.
      */
     public FastConfig getSubDirConfig() {
         return SUB_DIR_CONFIG;
     }
 
     /**
-     * Creates a subdirectory Config from a subdirectory name
-     * @param subDirectoryName Name of the subdirectory (created if it doesn't exist)
+     * Creates a subdirectory {@link FastConfig} from a subdirectory name.
+     * @param subDirectoryName Name of a subdirectory (created if it doesn't exist).
      */
     public void createSubDirConfig(String subDirectoryName) {
         this.SUB_DIR_CONFIG = new FastConfig(CONFIG_DIR_PATH.resolve(subDirectoryName));
     }
 
     /**
-     * Checks if the current Config target directory exists
-     * @return `true` if the current Config target directory exists, `false` otherwise
+     * Checks if the current Config target directory exists.
+     * @return `true` if the current Config target directory exists, otherwise `false`.
      */
     private boolean existsDir() {
         return new File(CONFIG_DIR_PATH.toUri()).exists();
     }
 
     /**
-     * Creates the current Config target directory
-     * @return `true` if the current Config target directory is created, `false` otherwise
+     * Creates the current Config target directory.
+     * @return `true` if the current Config target directory is created, otherwise `false`.
      */
     private boolean createDir() {
         return new File(CONFIG_DIR_PATH.toUri()).mkdir();
     }
 
     /**
-     * Creates a directory (based on Config location) if it doesn't exist
-     * @throws RuntimeException if the Config directory cannot not be created
+     * If it doesn't exist, creates a directory (based on Config location).
+     * @throws RuntimeException if the Config directory cannot not be created.
      */
     private void createDirIfNotExist() throws RuntimeException {
         if (!this.existsDir() && !this.createDir()) {
@@ -81,36 +85,37 @@ public class FastConfig {
     }
 
     /**
-     * Validates that a file name has a valid JSON extension
-     * @param fileName The file name
-     * @return the complete file name, otherwise `null` if the file name is not correct
+     * Checks if a file has a JSON extension.
+     * @param fileName The file name.
+     * @return the complete file name, otherwise `null` if the file name is not correct.
      */
     private String validateFileName(String fileName) {
         return (fileName.endsWith(".json")) ? fileName : fileName + ".json";
     }
 
     /**
-     * Gets or creates a new Config file based on attributes of the class provided
-     * @param configTemplate The class used as a Config template
-     * @return An instance of the `ConfigTemplate` class with values filled in
-     * @throws RuntimeException For errors in reading/writing files
+     * Creates a new {@link FastConfigFile} based on attributes of the provided class' instance.
+     * @param defaultInstance The class instance where the default values are set.
+     * @return A {@link FastConfigFile} that contains an instance of the current class, modifiable at runtime.
+     * @throws RuntimeException Thrown when errors occur in reading / writing files.
      */
-    public <T> FastConfigFile<T> getConfigFile(Class<T> configTemplate) throws RuntimeException {
-        File configFile = new File(CONFIG_DIR_PATH.resolve(validateFileName(CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_HYPHEN, configTemplate.getSimpleName()))).toUri());
+    public <T> FastConfigFile<T> getConfigFile(T defaultInstance) throws RuntimeException {
+        String className = defaultInstance.getClass().getSimpleName();
+        File configFile = new File(CONFIG_DIR_PATH.resolve(validateFileName(CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_HYPHEN, className))).toUri());
 
-        return new FastConfigFile<>(configFile, configTemplate, CONFIG_DIR_PATH);
+        return new FastConfigFile<>(configFile, defaultInstance, CONFIG_DIR_PATH);
     }
 
     /**
-     * Gets or creates a new Config file based on attributes of the class provided
-     * @param fileName The Config file name
-     * @param configTemplate The class used as a Config template
-     * @return An instance of the `configTemplate` class with values filled in
-     * @throws RuntimeException For errors in reading/writing files
+     * Creates a new {@link FastConfigFile} based on attributes of the provided class' instance.
+     * @param defaultInstance The class instance where the default values are set.
+     * @param fileName A custom name that will be used for the Config file.
+     * @return A {@link FastConfigFile} that contains an instance of the current class, modifiable at runtime.
+     * @throws RuntimeException Thrown when errors occur in reading / writing files.
      */
-    public <T> FastConfigFile<T> getConfigFile(Class<T> configTemplate, String fileName) throws RuntimeException {
+    public <T> FastConfigFile<T> getConfigFile(T defaultInstance, String fileName) throws RuntimeException {
         File configFile = new File(CONFIG_DIR_PATH.resolve(validateFileName(fileName)).toUri());
 
-        return new FastConfigFile<>(configFile, configTemplate, CONFIG_DIR_PATH);
+        return new FastConfigFile<>(configFile, defaultInstance, CONFIG_DIR_PATH);
     }
 }
