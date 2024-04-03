8b50f001fe2dc89a6bc9e578ebc116eb1e3ba693
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-04-03 02:08:22 +0200

    🎉 New version! Removed Fabric / Forge dependency on common so that it can be published and used without weird files. Changed version. Closes #2. Updated API Java Docs and API functions.

--- fabric/src/main/java/infinituum/fastconfigapi/fabric/FastConfigAPIFabric.java
+++ fabric/src/main/java/infinituum/fastconfigapi/fabric/FastConfigAPIFabric.java
@@ -1,11 +1,15 @@
 package infinituum.fastconfigapi.fabric;
 
-import infinituum.fastconfigapi.FastConfigAPI;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FastConfigAPIFabric implements ModInitializer {
+    public static final String MOD_ID = "fastconfigapi";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
-        FastConfigAPI.init();
+        LOGGER.info("FastConfigAPI has been initialized!");
     }
 }
