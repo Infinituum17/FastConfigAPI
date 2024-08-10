af6414ef5f94249e1cd96247f3839f42eef88115
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-08-10 12:51:56 +0200

    🔧 Added ConfigFile documentation

--- common/src/main/java/infinituum/fastconfigapi/api/config/ConfigFile.java
+++ common/src/main/java/infinituum/fastconfigapi/api/config/ConfigFile.java
@@ -1,9 +1,23 @@
 package infinituum.fastconfigapi.api.config;
 
+/**
+ * Interface that defines Config's methods to handle Files.
+ * <p>
+ * See {@link FastConfigFile}.
+ */
 public interface ConfigFile {
+    /**
+     * Method that saves the current state of the Fast Config class.
+     */
     void save();
 
+    /**
+     * Method that loads the current state (on disk) into the Fast Config class.
+     */
     void load();
 
+    /**
+     * Method that loads the default values of the Fast Config class.
+     */
     void loadDefault();
 }
