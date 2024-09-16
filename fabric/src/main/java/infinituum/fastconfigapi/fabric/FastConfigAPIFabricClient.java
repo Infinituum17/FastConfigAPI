4177e64d65f7cada71dc9c8782dd8851f0700a10
Infinituum <40234342+Infinituum17@users.noreply.github.com>
2024-09-16 15:30:09 +0200

    🔧 Added a bit of logging and updated README.md

--- fabric/src/main/java/infinituum/fastconfigapi/fabric/FastConfigAPIFabricClient.java
+++ fabric/src/main/java/infinituum/fastconfigapi/fabric/FastConfigAPIFabricClient.java
@@ -3,11 +3,13 @@ package infinituum.fastconfigapi.fabric;
 import infinituum.fastconfigapi.FastConfigs;
 import infinituum.fastconfigapi.api.annotations.FastConfig;
 import infinituum.fastconfigapi.fabric.utils.ConfigScanner;
+import infinituum.fastconfigapi.utils.Global;
 import net.fabricmc.api.ClientModInitializer;
 
 public final class FastConfigAPIFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         FastConfigs.register(ConfigScanner.getSidedConfigs(FastConfig.Side.CLIENT));
+        Global.LOGGER.info("FastConfigAPI-client was successfully loaded");
     }
 }
--- forge/src/main/java/infinituum/fastconfigapi/forge/events/LifecycleEventHandlers.java
+++ forge/src/main/java/infinituum/fastconfigapi/forge/events/LifecycleEventHandlers.java
@@ -3,6 +3,7 @@ package infinituum.fastconfigapi.forge.events;
 import infinituum.fastconfigapi.FastConfigs;
 import infinituum.fastconfigapi.api.annotations.FastConfig;
 import infinituum.fastconfigapi.forge.utils.ConfigScanner;
+import infinituum.fastconfigapi.utils.Global;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -16,15 +17,18 @@ public final class LifecycleEventHandlers {
     @SubscribeEvent
     public static void commonHandler(FMLCommonSetupEvent event) {
         FastConfigs.register(ConfigScanner.getSidedConfigs(FastConfig.Side.COMMON));
+        Global.LOGGER.info("FastConfigAPI-common was successfully loaded");
     }
 
     @SubscribeEvent
     public static void clientHandler(FMLClientSetupEvent event) {
         FastConfigs.register(ConfigScanner.getSidedConfigs(FastConfig.Side.CLIENT));
+        Global.LOGGER.info("FastConfigAPI-client was successfully loaded");
     }
 
     @SubscribeEvent
     public static void serverHandler(FMLDedicatedServerSetupEvent event) {
         FastConfigs.register(ConfigScanner.getSidedConfigs(FastConfig.Side.SERVER));
+        Global.LOGGER.info("FastConfigAPI-server was successfully loaded");
     }
 }
