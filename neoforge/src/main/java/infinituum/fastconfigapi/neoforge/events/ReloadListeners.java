7ea68d67256ececf4550b3a7a9b9ff7ea2265c2b
Infinituum <40234342+Infinituum17@users.noreply.github.com>
2025-10-08 15:57:42 +0200

    🐳 Small fix in neoforge

--- neoforge/src/main/java/infinituum/fastconfigapi/neoforge/events/ReloadListeners.java
+++ neoforge/src/main/java/infinituum/fastconfigapi/neoforge/events/ReloadListeners.java
@@ -3,6 +3,7 @@ package infinituum.fastconfigapi.neoforge.events;
 import infinituum.fastconfigapi.api.annotations.FastConfig;
 import infinituum.fastconfigapi.utils.ConfigResourceManagerReloadListener;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent;
@@ -14,7 +15,7 @@ public class ReloadListeners {
     private static final ResourceLocation CLIENT_CONFIG_RESOURCE = ResourceLocation.fromNamespaceAndPath(MOD_ID, "client_config_resource");
     private static final ResourceLocation SERVER_CONFIG_RESOURCE = ResourceLocation.fromNamespaceAndPath(MOD_ID, "server_config_resource");
 
-    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.GAME)
+    @EventBusSubscriber(modid = MOD_ID, value = Dist.DEDICATED_SERVER)
     public static class Server {
         @SubscribeEvent
         public static void addReloadListeners(AddServerReloadListenersEvent event) {
@@ -22,7 +23,7 @@ public class ReloadListeners {
         }
     }
 
-    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+    @EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
     public static class Client {
         @SubscribeEvent
         public static void addReloadListeners(AddClientReloadListenersEvent event) {
